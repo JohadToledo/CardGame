8690aa5863bf3cdc5c8a97a850c30992523d2997
Johad Toledo <johadtledo@gmail.com>
2022-02-19 13:22:18 +0100

    edit the js file

--- public/data.js
+++ public/data.js
@@ -1,59 +1,59 @@
 let data = [
     {name: "The Fool",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-fool.jpg",
-    description: "The Tarot Fool card is a jolly one, judging from the image on it. He may be a fool, but doesn't he look happy? Maybe that's what it takes to be joyous in this world. Ignorance is bliss."
+    description: "The Tarot Fool card is a jolly one, judging from the image on it. He may be a fool, but doesn't he look happy? Maybe that's what it takes to be joyous in this world."
 },
     {name: "The Magician",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-magician.jpg",
-    description: "  Among devoted Tarot users, the Magician is often the favorite card. That's because already using the Tarot is a kind of magic - opening the door to a reality yet to be explained in scientific terms, if that's ever going to be possible."
+    description: "  Among devoted Tarot users, the Magician is often the favorite card. That's because already using the Tarot is a kind of magic."
 },
     {name: "The High Priestess",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-highpriestess.jpg",
-    description: "The Tarot High Priestess is the ruler of what's hidden and secret, the essence behind the scenes. She knows what really goes on. The High Priestess is not that eager to reveal it, though, at least not to those who are fooled by the superficial. You have to deserve the revelations to which she holds the keys."
+    description: "The Tarot High Priestess is the ruler of what's hidden and secret, the essence behind the scenes. She knows what really goes on."
 },
     {name: "The Empress",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-empress.jpg",
-    description: "The Tarot Empress is a ruler, make no mistake about that, but a gentle one getting her will by peaceful means. She suggests and convinces, making her choices seem like the only reasonable ones. Quite often they are. If you follow the Tarot Empress you will have a pleasant ride, but if you oppose her it will all be uphill."
+    description: "The Tarot Empress is a ruler, make no mistake about that, but a gentle one getting her will by peaceful means."
 },
     {name: "The Emperor",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-emperor.jpg",
-    description: "The Tarot Emperor has real concrete power in abundance. He's no politician in need of allies and careful diplomacy. He's raw force. Anybody dealing with him should yield or else perish. On the Tarot Emperor card, he sits firmly on the throne, but in armor - he will stand up and strike at the moment he is challenged. He tolerates nothing but obedience."
+    description: "The Tarot Emperor has real concrete power in abundance. He's no politician in need of allies and careful diplomacy."
 },
     {name: "The Hierophant",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-hierophant.jpg",
-    description: "There's no doubt that the elevated figure on the Tarot Hierophant card is the pope. The word hierophant is Greek, meaning someone who shows the holy. The card is sometimes called The High Priest, which is just about the same: the one claiming to be closest to what's holy, thereby kind of holy himself, too. But that's far from certain."
+    description: "There's no doubt that the elevated figure on the Tarot Hierophant card is the pope. The word hierophant is Greek, meaning someone who shows the holy."
 },
     {name: "The Lovers",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-lovers.jpg",
-    description: " Except for the nudity, the picture of the Lovers Major Arcana Tarot card is quite innocent, maybe even virginal. That's because of the time and culture in which the Tarot deck of cards was created."
+    description: " Except for the nudity, the picture of the Lovers Major Arcana Tarot card is quite innocent, maybe even virginal."
 },
     {name: "The Chariot",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-chariot.jpg",
-    description: " The Tarot Chariot card is about triumph in worldly matters. Like Caesar returning from a successful campaign, riding a chariot when receiving the cheers of the Romans. A moment of greatness, but also a risk of megalomania. Success can destroy character quicker than adversity builds it."
+    description: " The Tarot Chariot card is about triumph in worldly matters. Like Caesar returning from a successful campaign, riding a chariot when receiving the cheers of the Romans."
 },
     {name: "Strength",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-strength.jpg",
-    description: "The strength on the picture of the Tarot card seems delicate: A maiden pure at heart holds the jaws of a lion, making it harmless. That's real strength, far beyond what mere muscles can accomplish. It's so powerful because it has an ideal."
+    description: "The strength on the picture of the Tarot card seems delicate: A maiden pure at heart holds the jaws of a lion, making it harmless. That's real strength."
 },
     {name: "The Hermit",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-hermit.jpg",
-    description: "The Tarot card picture of the Hermit is a gloomy one. That's because of the solitude the card represents. Although it's voluntary in the case of a hermit, solitude is still somewhat sad. It's not chosen as a wish of separating oneself from fellow human beings, but for a certain purpose where being alone is instrumental."
+    description: "The Tarot card picture of the Hermit is a gloomy one. That's because of the solitude the card represents. Although it's voluntary in the case of a hermit."
 },
     {name: "The Wheel of Fortune",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-wheeloffortune.jpg",
-    description: "The Wheel of Fortune Tarot card image looks like a roulette. The outcome is uncertain, no matter what you do, so you can only hope to be lucky. The one thing to be expected is surprise."
+    description: "The Wheel of Fortune Tarot card image looks like a roulette. The outcome is uncertain, no matter what you do, so you can only hope to be lucky."
 },
     {name: "Justice",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-justice.jpg",
-    description: "The Justice of the Tarot is not blind. There's the sword representing the sharp firmness of the law, and the scales by which to judge the actions of men, as carried by every image of Justitia. But the Tarot Justice has no blindfold, the guarantee that all people are treated as equals before the law."
+    description: "The Justice of the Tarot is not blind. There's the sword representing the sharp firmness of the law, and the scales by which to judge the actions of men."
 },
     {name: "The Hanged Man",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-hangedman.jpg",
-    description: " The Tarot Hanged Man card is about personal sacrifice, of course. But this figure seems not to be broken, although hanging upside-down. He will bounce right back, as soon as he manages to get free. It's not that he heals easily."
+    description: " The Tarot Hanged Man card is about personal sacrifice, of course. But this figure seems not to be broken, although hanging upside-down."
 },
     {name: "Death",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-death.jpg",
-    description: "Death is, no doubt, the most terrifying of the Tarot cards to get at a reading. Therefore, guides to the Tarot usually point out that it's not necessarily about death, but some significant loss, change, or revelation."
+    description: "Death is, no doubt, the most terrifying of the Tarot cards to get at a reading. Therefore, guides to the Tarot usually point out that it's not necessarily about death"
 },
     {name: "Temperance",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-temperance.jpg",
@@ -61,19 +61,19 @@ let data = [
 },
     {name: "The Devil",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-devil.jpg",
-    description: "The Tarot Devil is all about temptation and how we're often enslaved by our lust. There are many other devils, pointing to other vices and weaknesses of mankind. This one focuses on sexual aspects, which makes it much less frightening."
+    description: "The Tarot Devil is all about temptation and how we're often enslaved by our lust. There are many other devils, pointing to other vices and weaknesses of mankind."
 },
     {name: "The Tower",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-tower.jpg",
-    description: "The Tower is evidently a card indicating disaster. The picture shows that clearly. But what leads to the disaster? One legendary tower explains it - that of Babel, surely inspiring both the image and the meaning of the Tarot Tower card. Babel was built to reach heaven."
+    description: "The Tower is evidently a card indicating disaster. The picture shows that clearly. But what leads to the disaster?"
 },
     {name: "The Star",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-star.jpg",
-    description: " The Star is very distant and mysterious. Although the sky on the Tarot Star card image is light blue, the Star is a nocturnal being. Its shimmering light inspires contemplation and dreaming. It's emotion is melancholia. "
+    description: " The Star is very distant and mysterious. Although the sky on the Tarot Star card image is light blue, the Star is a nocturnal being.  "
 },
     {name: "The Moon",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-moon.jpg",
-    description: " The Tarot Moon card stands for longing, the needs of the soul, just like the moon does in astrology. The link to astrology, which is older by far than the Tarot, is obvious in several ways on the card's image."
+    description: " The Tarot Moon card stands for longing, the needs of the soul, just like the moon does in astrology. "
 },
     {name: "The Sun",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-sun.jpg",
@@ -81,10 +81,10 @@ let data = [
 },
     {name: "Judgement",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-judgement.jpg",
-    description: " The picture on the Tarot Judgement card makes no secret of what judgment it refers to: the Last Judgment, when all the people who ever lived are awakened and sent either to Heaven or to Hell for eternity."
+    description: " The picture on the Tarot Judgement card makes no secret of what judgment it refers to: the Last Judgment"
 },
     {name: "The World",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-world.jpg",
-    description: "The World is a place of infinite opportunity, there for the grabbing. You can conquer it and you can lose it. The Tarot World card opens this vast resource, but who has the embrace wide enough to encompass it?"
+    description: "The World is a place of infinite opportunity, there for the grabbing. You can conquer it and you can lose it."
 }
 ];
--- public/index.js
+++ public/index.js
@@ -13,6 +13,16 @@ let desc2 = document.getElementById('desc2')
 let pic3 = document.getElementById('pic3')
 let text3 = document.getElementById('text3')
 let desc3 = document.getElementById('desc3')
+let pic4 = document.getElementById('pic4')
+let text4 = document.getElementById('text4')
+let desc4 = document.getElementById('desc4')
+let pic5 = document.getElementById('pic5')
+let text5 = document.getElementById('text5')
+let desc5 = document.getElementById('desc5')
+let pic6 = document.getElementById('pic6')
+let text6 = document.getElementById('text6')
+let desc6 = document.getElementById('desc6')
+let previousGames = []
 
 let randomNumber = (min, max) => {
     random = Math.round(Math.random() * (max - min) + min)
@@ -31,28 +41,44 @@ let play = () => {
             play()
         }else{
             console.log(results)
-            resultIterate()
+            resultIterate(results)
+            previousGames.push(results)
         }
 }
 
-let resultIterate = ()=>{
-pic1.setAttribute('src', results[0].img)
-pic2.setAttribute('src', results[1].img)
-pic3.setAttribute('src', results[2].img)
-text1.innerHTML = results[0].name
-text2.innerHTML = results[1].name
-text3.innerHTML = results[2].name
-desc1.innerHTML = results[0].description
-desc2.innerHTML = results[1].description
-desc3.innerHTML = results[2].description
+let resultIterate = (completedGame)=>{
+pic1.setAttribute('src', completedGame[0].img)
+pic2.setAttribute('src', completedGame[1].img)
+pic3.setAttribute('src', completedGame[2].img)
+pic4.setAttribute('src', completedGame[3].img)
+pic5.setAttribute('src', completedGame[4].img)
+pic6.setAttribute('src', completedGame[5].img)
+text1.innerHTML = completedGame[0].name
+text2.innerHTML = completedGame[1].name
+text3.innerHTML = completedGame[2].name
+text4.innerHTML = completedGame[3].name
+text5.innerHTML = completedGame[4].name
+text6.innerHTML = completedGame[5].name
+desc1.innerHTML = completedGame[0].description
+desc2.innerHTML = completedGame[1].description
+desc3.innerHTML = completedGame[2].description
+desc4.innerHTML = completedGame[3].description
+desc5.innerHTML = completedGame[4].description
+desc6.innerHTML = completedGame[5].description
 }
+
+let playPrevious =(gameN)=>{
+    resultIterate(previousGames[gameN])
+    showResult()
+}
+
 let resetAndPlay =()=>{
     games = 0
     results = []
     play()
 }
 
-luck.addEventListener('click', () => {
+let showResult =()=>{
     home.style.display = 'none';
     carousel.style.display= 'none';
     animation.style.display= 'flex'
@@ -61,4 +87,13 @@ luck.addEventListener('click', () => {
         carousel.style.display= 'flex'
     }, 3000)
     resetAndPlay()
+}
+
+luck.addEventListener('click', () => {
+    showResult()
 })
+
+let back =()=>{
+    home.style.display= 'flex'
+    carousel.style.display= 'none'
+}
