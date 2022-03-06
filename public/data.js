f4949c69e9b708d166dc939b8739612d17083b13
Johad Toledo <johadtledo@gmail.com>
2022-03-06 21:54:44 +0100

    add match function

--- public/data.js
+++ public/data.js
@@ -7,106 +7,106 @@ let data = [
     {name: "The Magician",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-magician.jpg",
     description: "  Among devoted Tarot users, the Magician is often the favorite card. That's because already using the Tarot is a kind of magic.",
-    match: 0 
+    match: 2 
     },
     {name: "The High Priestess",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-highpriestess.jpg",
     description: "The Tarot High Priestess is the ruler of what's hidden and secret, the essence behind the scenes. She knows what really goes on.",
-    match: -1
+    match: 3
     },
     {name: "The Empress",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-empress.jpg",
     description: "The Tarot Empress is a ruler, make no mistake about that, but a gentle one getting her will by peaceful means.",
-    match: 1 
+    match: 4 
     },
     {name: "The Emperor",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-emperor.jpg",
     description: "The Tarot Emperor has real concrete power in abundance. He's no politician in need of allies and careful diplomacy.", 
-    match: 0 
+    match: 5 
     },
     {name: "The Hierophant",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-hierophant.jpg",
     description: "There's no doubt that the elevated figure on the Tarot Hierophant card is the pope. The word hierophant is Greek, meaning someone who shows the holy.", 
-    match: -1 
+    match: 6 
     },
     {name: "The Lovers",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-lovers.jpg",
     description: " Except for the nudity, the picture of the Lovers Major Arcana Tarot card is quite innocent, maybe even virginal.", 
-    match: 1 
+    match: 7 
     },
     {name: "The Chariot",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-chariot.jpg",
     description: " The Tarot Chariot card is about triumph in worldly matters. Like Caesar returning from a successful campaign, riding a chariot when receiving the cheers of the Romans.", 
-    match: 0 
+    match: 8 
     },
     {name: "Strength",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-strength.jpg",
     description: "The strength on the picture of the Tarot card seems delicate: A maiden pure at heart holds the jaws of a lion, making it harmless. That's real strength.", 
-    match: -1 
+    match: 9 
     },
     {name: "The Hermit",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-hermit.jpg",
     description: "The Tarot card picture of the Hermit is a gloomy one. That's because of the solitude the card represents. Although it's voluntary in the case of a hermit.", 
-    match: 1 
+    match: 10 
     },
     {name: "The Wheel of Fortune",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-wheeloffortune.jpg",
     description: "The Wheel of Fortune Tarot card image looks like a roulette. The outcome is uncertain, no matter what you do, so you can only hope to be lucky.", 
-    match: 0 
+    match: 11 
     },
     {name: "Justice",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-justice.jpg",
     description: "The Justice of the Tarot is not blind. There's the sword representing the sharp firmness of the law, and the scales by which to judge the actions of men.", 
-    match: -1 
+    match: 12 
     },
     {name: "The Hanged Man",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-hangedman.jpg",
     description: " The Tarot Hanged Man card is about personal sacrifice, of course. But this figure seems not to be broken, although hanging upside-down.", 
-    match: 1 
+    match: 13 
     },
     {name: "Death",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-death.jpg",
     description: "Death is, no doubt, the most terrifying of the Tarot cards to get at a reading. Therefore, guides to the Tarot usually point out that it's not necessarily about death", 
-    match: 0 
+    match: 14 
     },
     {name: "Temperance",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-temperance.jpg",
     description: " The image of the Temperance Tarot card is one of stillness. The simple explanation of Temperance is moderation, but there's nothing simple about it.", 
-    match: -1 
+    match: 15 
     },
     {name: "The Devil",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-devil.jpg",
     description: "The Tarot Devil is all about temptation and how we're often enslaved by our lust. There are many other devils, pointing to other vices and weaknesses of mankind.", 
-    match: 1 
+    match: 16 
     },
     {name: "The Tower",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-tower.jpg",
     description: "The Tower is evidently a card indicating disaster. The picture shows that clearly. But what leads to the disaster?", 
-    match: 0 
+    match: 17 
     },
     {name: "The Star",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-star.jpg",
     description: " The Star is very distant and mysterious. Although the sky on the Tarot Star card image is light blue, the Star is a nocturnal being.  ", 
-    match: -1 
+    match: 18 
     },
     {name: "The Moon",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-moon.jpg",
     description: " The Tarot Moon card stands for longing, the needs of the soul, just like the moon does in astrology. ", 
-    match: 1 
+    match: 19 
     },
     {name: "The Sun",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-sun.jpg",
     description: "The sun brings light and life to our whole planet. It's the source of seemingly endless energy, without which we couldn't exist. ", 
-    match: 0 
+    match: 20 
     },
     {name: "Judgement",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-judgement.jpg",
     description: " The picture on the Tarot Judgement card makes no secret of what judgment it refers to: the Last Judgment", 
-    match: -1 
+    match: 21 
     },
     {name: "The World",
     img: "https://www.tarotcardmeanings.net/images/tarotcards/tarot-world.jpg",
     description: "The World is a place of infinite opportunity, there for the grabbing. You can conquer it and you can lose it.",
-    match: -1
+    match: 22
     }
 ];
--- public/main.js
+++ public/main.js
@@ -12,10 +12,17 @@ let results = document.querySelector('.results')
 let carouselInner = document.querySelector('.carousel-inner')
 let carouselGame = document.querySelector('#carouselGame')
 let carouselPlayerName = document.querySelector('#carouselPlayerName')
+let btnContinue = document.querySelector('#btnContinue')
 
-
+let listX = document.querySelector('.listX')
+let listXPlayerName = document.querySelector('.listX h3')
+let listXPlayerCards = document.querySelector('.listX ul')
+let listZ = document.querySelector('.listZ')
+let listZPlayerName = document.querySelector('.listZ h3')
+let listZPlayerCards = document.querySelector('.listZ ul')
 
 let allGames = []
+let currentGame = []
 
 const showPage = (page) =>{
     section.forEach((page) =>{
@@ -31,7 +38,6 @@ const showPage = (page) =>{
 }
 
 const startGame = () =>{
-    let currentGame = []
     let random = 0
     let slide = ''
     let active = ''
@@ -63,6 +69,30 @@ carouselGame.addEventListener('slide.bs.carousel', function (e) {
     }if(cardNumber > 3 && cardNumber <= 6){
         carouselPlayerName.innerHTML = `Card ${cardNumber - 3}/3 of ${playerZ}`
     }
+  })
+
+  const showResults = () =>{
+      let sum = 0
+      currentGame.forEach((card, index) =>{
+        if(index < 3){
+            listXPlayerCards.innerHTML += `<li><img class='' src='${card.img}'/></li>`
+        }else{
+            listZPlayerCards.innerHTML += `<li><img class='' src='${card.img}'/></li>`
+        }
+        sum = sum + card.match
+    })
+    if(sum % 2 == 0){
+        console.log('match YES')
+    }else console.log('match NO')
+    
+    console.log(sum)
+    listXPlayerName.innerHTML = `${playerX}`
+    listZPlayerName.innerHTML = `${playerZ}`
+}
+
+  btnContinue.addEventListener('click', (e)=>{
+    showResults()
+    showPage(results)
   })
 
 form.addEventListener('submit', (e) => {
